4e28b0d3cc97c915fa3ce3cb8117459d3dc332c9
devsabbirhossain <sabbir2dev@gmail.com>
2024-03-24 13:55:47 +0600

    add dynamic data to list

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import ActionTask from "./taskBoard/ActionTask";
 import ListTask from "./taskBoard/ListTask";
 import SearchTask from "./taskBoard/SearchTask";
 
 const TaskBoard = () => {
+  const taskList = {
+    id: crypto.randomUUID(),
+    title: "Game Development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
+    tags: ["Unity", "C#", "Graphic"],
+    priority: "High",
+    isFavorite: true,
+  };
+  
+  const [tasks, setTasks] = useState([taskList]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +24,7 @@ const TaskBoard = () => {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <ActionTask />
-          <ListTask />
+          <ListTask tasks={tasks} />
         </div>
       </div>
     </section>
